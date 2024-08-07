afed285dcc1aa50cb4fab1b2d77b0d80d6e12646
Nikita Volkov <nikita.y.volkov@mail.ru>
2024-08-07 07:32:36 +0300

    Implement the top level template

--- codegens/Modeliero/Codegens/Haskell.hs
+++ codegens/Modeliero/Codegens/Haskell.hs
@@ -4,6 +4,7 @@ module Modeliero.Codegens.Haskell where
 
 import Coalmine.Prelude
 import Coalmine.Slug qualified as Slug
+import Data.Text qualified as Text
 import Modeliero.Codegens.Haskell.Dsls.Code qualified as Code
 import Modeliero.Codegens.Haskell.Dsls.InModule qualified as InModule
 import Modeliero.Codegens.Haskell.Dsls.Namespace qualified as Namespace
@@ -11,6 +12,7 @@ import Modeliero.Codegens.Haskell.Dsls.Package qualified as Package
 import Modeliero.Codegens.Haskell.IntegratedTemplates.ProductModelModule qualified as IntegratedTemplates.ProductModelModule
 import Modeliero.Codegens.Haskell.Params qualified as Params
 import Modeliero.Codegens.Haskell.Templates.ProductModelModule qualified as Templates.ProductModelModule
+import Modeliero.Codegens.Haskell.Templates.ReexportsModule qualified as Templates.ReexportsModule
 
 type Params = Params.Model
 
@@ -19,25 +21,49 @@ type Result = [(FilePath, Text)]
 
 compile :: Params -> Result
 compile params =
-  error "TODO"
+  Package.compilePackageFiles package
+    & fmap (\file -> (file.path, file.content))
   where
-    modelsNamespace =
-      [ params.name & Slug.toUpperCamelCaseText,
-        "Types"
-      ]
+    package =
+      Package.Package
+        { name = params.name & Slug.toSpinalCaseText,
+          synopsis = "Generated model package",
+          modules
+        }
+    modules =
+      Package.Modules
+        { public =
+            [ reexportsModule
+            ],
+          private =
+            modelModules
+        }
+    rootNamespace =
+      [params.name & Slug.toUpperCamelCaseText]
+    typesNamespace =
+      rootNamespace <> ["Types"]
     importAliases = []
     modelModules =
       params.types
         & fmap
           ( \type_ ->
-              let moduleName = modelsNamespace <> [type_.name & Slug.toUpperCamelCaseText]
+              let moduleName = typesNamespace <> [type_.name & Slug.toUpperCamelCaseText]
                in case type_.definition of
                     Params.ProductTypeDefinition fields ->
                       InModule.compileToModule moduleName importAliases
                         $ IntegratedTemplates.ProductModelModule.compile
-                          modelsNamespace
+                          typesNamespace
                           params.instances
                           type_.name
                           type_.docs
                           fields
           )
+    reexportsModule =
+      InModule.compileToModule rootNamespace importAliases
+        $ Templates.ReexportsModule.compile
+          Templates.ReexportsModule.Params
+            { models =
+                modelModules
+                  & fmap (.name)
+                  & fmap (Text.intercalate ".")
+            }
