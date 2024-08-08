fafcc696fde6e1bab6ba80882851705be074e0fd
Nikita Volkov <nikita.y.volkov@mail.ru>
2024-08-08 07:42:10 +0300

    Implement Refined data type template

---
+++ codegens/Modeliero/Codegens/Haskell/Templates/RefinedModelModule/Templates/DataTypeDeclaration.hs
@@ -0,0 +1,57 @@
+module Modeliero.Codegens.Haskell.Templates.RefinedModelModule.Templates.DataTypeDeclaration where
+
+import Coalmine.MultilineTextBuilder qualified as TextBlock
+import Coalmine.Prelude
+import CodegenKit.Legacy.ByLanguage.Haskell.Snippets qualified as Snippets
+import Data.Text qualified as Text
+import Modeliero.Codegens.Haskell.Dsls.InModule
+import Modeliero.Codegens.Haskell.Imports qualified as Imports
+
+data Params = Params
+  { name :: TextBlock,
+    haddock :: Text,
+    baseType :: TextBlock,
+    derivings :: Derivings
+  }
+
+type Result = InModule TextBlock
+
+data Derivings = Derivings
+  { show :: Bool,
+    eq :: Bool,
+    ord :: Bool,
+    generic :: Bool
+  }
+
+compile :: Params -> Result
+compile params = do
+  derivingSplices <- compileDerivings params.derivings
+  pure
+    ( mconcat
+        [ filtered (not . Text.null) (flip mappend "\n" . Snippets.prefixHaddock) params.haddock,
+          [j|
+            newtype ${params.name} = ${params.name}
+              { base :: ${params.baseType}
+              }
+          |],
+          case derivingSplices of
+            [] -> mempty
+            _ -> "\n  deriving (" <> TextBlock.intercalate ", " derivingSplices <> ")"
+        ]
+    )
+
+compileDerivings :: Derivings -> InModule [TextBlock]
+compileDerivings derivings = do
+  sequence
+    $ catMaybes
+      [ compileDeriving derivings.show "Show" (requestImport Imports.basePrelude),
+        compileDeriving derivings.eq "Eq" (requestImport Imports.basePrelude),
+        compileDeriving derivings.ord "Ord" (requestImport Imports.basePrelude),
+        compileDeriving derivings.generic "Generic" (requestImport Imports.baseGenerics)
+      ]
+
+compileDeriving :: Bool -> Text -> InModule Text -> Maybe (InModule TextBlock)
+compileDeriving enabled name importRequest =
+  if enabled
+    then Just (importRequest <&> \qfr -> to qfr <> to name)
+    else Nothing
