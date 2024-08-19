0624502526c5fff92e717adda2b220c7d7acac62
Nikita Volkov <nikita.y.volkov@mail.ru>
2024-08-19 09:01:31 +0300

    Clean up

--- sources/Modeliero/Sources/AsyncApi/Preludes/Parser.hs
+++ sources/Modeliero/Sources/AsyncApi/Preludes/Parser.hs
@@ -1,5 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-binds -Wno-unused-imports -Wno-name-shadowing -Wno-incomplete-patterns #-}
-
 module Modeliero.Sources.AsyncApi.Preludes.Parser
   ( module Modeliero.Sources.AsyncApi.Preludes.Parser,
     module Modeliero.Codegens.Haskell.Params,
@@ -9,7 +7,6 @@ where
 
 import Coalmine.Prelude
 import Data.OpenApi qualified as OpenApi
-import Modeliero.AsyncApi qualified as AsyncApi
 import Modeliero.Codegens.Haskell.Params
 
 type SchemaParser i e o =
