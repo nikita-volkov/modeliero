54d83a2ad5cf03cda36b5ed476120c2027b5b2c9
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-01-31 08:14:54 +0300

    Provide support for enums with string type

--- sources/Modeliero/Sources/AsyncApi/ParserOf/OutlineSchema.hs
+++ sources/Modeliero/Sources/AsyncApi/ParserOf/OutlineSchema.hs
@@ -28,115 +28,121 @@ type Error = Json
 parse :: SchemaContext -> Input -> Either Error Output
 parse schemaContext input = do
   directDefinitionIfPossible <-
-    label "outline" case input._schemaType of
-      Nothing ->
-        asum
-          [ input._schemaOneOf <&> \oneOf ->
-              do
-                OneOfParser.Output variants unparsedTypeDeclarations <-
-                  oneOf
-                    & nest "one-of" (jsonifyErrors OneOfParser.parse) schemaContext
-                pure (SumTypeDefinition variants, unparsedTypeDeclarations),
-            input._schemaEnum
-              <&> fmap (,[])
-              . fmap EnumTypeDefinition
-              . EnumVariants.parse schemaContext
-          ]
-          & sequence
-      Just schemaType ->
-        case schemaType of
-          Input.OpenApiObject -> do
-            input._schemaProperties
-              & InsOrd.toList
-              & traverse
-                ( \(nameInput, valueReferencedSchema) -> first (Json.tagged nameInput) do
-                    name <-
-                      nameInput
-                        & Cases.spinalize
-                        & specialize
-                        & first toJSON
-                        & assocWithInput nameInput
-                        & label "name-slug"
-                    referencedSchemaOutput <-
-                      nest "value" ReferencedSchemaParser.parse schemaContext valueReferencedSchema
-                    case referencedSchemaOutput of
-                      ReferencedSchemaParser.ReferenceOutput _ref slug ->
-                        pure
-                          ( Field
-                              { name,
-                                jsonName = nameInput,
-                                docs = "",
-                                type_ =
-                                  packPlainType nameInput
-                                    $ LocalPlainType
-                                    $ slug
-                              },
-                            []
-                          )
-                      ReferencedSchemaParser.InlineOutput inlineSchema -> do
-                        propertySchema <-
-                          appendContextReference
-                            name
-                            (nest "inline-value" InlineSchemaParser.parse)
-                            schemaContext
-                            inlineSchema
-                        pure
-                          ( Field
-                              { name,
-                                jsonName = nameInput,
-                                docs = propertySchema.docs,
-                                type_ =
-                                  propertySchema.valueType
-                                    & packValueType nameInput
-                              },
-                            propertySchema.typeDeclarations
-                          )
-                )
-              & label "properties"
-              & fmap unzip
-              & fmap
-                ( \(fields, typeDeclarations) ->
-                    ( ProductTypeDefinition schemaContext.anonymizable fields,
-                      concat typeDeclarations
-                    )
-                )
-              & fmap Just
-            where
-              packPlainType fieldName =
-                packValueType fieldName . PlainValueType
-              packValueType fieldName =
-                if elem fieldName input._schemaRequired
-                  then id
-                  else MaybeValueType
-          Input.OpenApiString ->
-            case input._schemaFormat of
-              Nothing ->
-                TextRestrictions
-                  { regexp = Nothing,
-                    minLength =
-                      input._schemaMinLength
-                        & fmap fromIntegral
-                        & fromMaybe 0,
-                    maxLength =
-                      input._schemaMaxLength
-                        & fmap fromIntegral
-                        & fromMaybe schemaContext.config.defaultTextMaxLength,
-                    charsetRangeList = Nothing
-                  }
-                  & TextRefinement
-                  & Left
-                  & ( \wrappedType ->
-                        NewtypeDefinition
-                          { wrappedType,
-                            anonymizable = schemaContext.anonymizable
-                          }
-                    )
-                  & NewtypeTypeDefinition
-                  & (,[])
-                  & Just
-                  & pure
-              _ -> pure Nothing
-          _ -> pure Nothing
+    label "outline" case input._schemaEnum of
+      Just schemaEnumValues ->
+        schemaEnumValues
+          & EnumVariants.parse schemaContext
+          & fmap ((,[]) . EnumTypeDefinition)
+          & fmap Just
+      Nothing -> case input._schemaType of
+        Nothing ->
+          asum
+            [ input._schemaOneOf <&> \oneOf ->
+                do
+                  OneOfParser.Output variants unparsedTypeDeclarations <-
+                    oneOf
+                      & nest "one-of" (jsonifyErrors OneOfParser.parse) schemaContext
+                  pure (SumTypeDefinition variants, unparsedTypeDeclarations),
+              input._schemaEnum
+                <&> fmap (,[])
+                . fmap EnumTypeDefinition
+                . EnumVariants.parse schemaContext
+            ]
+            & sequence
+        Just schemaType ->
+          case schemaType of
+            Input.OpenApiObject -> do
+              input._schemaProperties
+                & InsOrd.toList
+                & traverse
+                  ( \(nameInput, valueReferencedSchema) -> first (Json.tagged nameInput) do
+                      name <-
+                        nameInput
+                          & Cases.spinalize
+                          & specialize
+                          & first toJSON
+                          & assocWithInput nameInput
+                          & label "name-slug"
+                      referencedSchemaOutput <-
+                        nest "value" ReferencedSchemaParser.parse schemaContext valueReferencedSchema
+                      case referencedSchemaOutput of
+                        ReferencedSchemaParser.ReferenceOutput _ref slug ->
+                          pure
+                            ( Field
+                                { name,
+                                  jsonName = nameInput,
+                                  docs = "",
+                                  type_ =
+                                    packPlainType nameInput
+                                      $ LocalPlainType
+                                      $ slug
+                                },
+                              []
+                            )
+                        ReferencedSchemaParser.InlineOutput inlineSchema -> do
+                          propertySchema <-
+                            appendContextReference
+                              name
+                              (nest "inline-value" InlineSchemaParser.parse)
+                              schemaContext
+                              inlineSchema
+                          pure
+                            ( Field
+                                { name,
+                                  jsonName = nameInput,
+                                  docs = propertySchema.docs,
+                                  type_ =
+                                    propertySchema.valueType
+                                      & packValueType nameInput
+                                },
+                              propertySchema.typeDeclarations
+                            )
+                  )
+                & label "properties"
+                & fmap unzip
+                & fmap
+                  ( \(fields, typeDeclarations) ->
+                      ( ProductTypeDefinition schemaContext.anonymizable fields,
+                        concat typeDeclarations
+                      )
+                  )
+                & fmap Just
+              where
+                packPlainType fieldName =
+                  packValueType fieldName . PlainValueType
+                packValueType fieldName =
+                  if elem fieldName input._schemaRequired
+                    then id
+                    else MaybeValueType
+            Input.OpenApiString ->
+              case input._schemaFormat of
+                Nothing ->
+                  TextRestrictions
+                    { regexp = Nothing,
+                      minLength =
+                        input._schemaMinLength
+                          & fmap fromIntegral
+                          & fromMaybe 0,
+                      maxLength =
+                        input._schemaMaxLength
+                          & fmap fromIntegral
+                          & fromMaybe schemaContext.config.defaultTextMaxLength,
+                      charsetRangeList = Nothing
+                    }
+                    & TextRefinement
+                    & Left
+                    & ( \wrappedType ->
+                          NewtypeDefinition
+                            { wrappedType,
+                              anonymizable = schemaContext.anonymizable
+                            }
+                      )
+                    & NewtypeTypeDefinition
+                    & (,[])
+                    & Just
+                    & pure
+                _ -> pure Nothing
+            _ -> pure Nothing
 
   (definition, unparsedTypeDeclarations) <-
     case directDefinitionIfPossible of
